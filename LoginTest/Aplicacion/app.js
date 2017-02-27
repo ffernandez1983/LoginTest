881e9ff9c81d6c493009af590a32550d6523effe
ffernandez1983 <ffernandez1983@gmail.com>
2017-02-27 13:08:47 +0100

    Add project files.

---
+++ LoginTest/Aplicacion/app.js
@@ -0,0 +1,15 @@
+﻿
+angular
+    .module('app',
+    [
+		'cabecera.Directiva'
+    ])
+    .config(function () {
+
+    })
+	/*.config([/*'inyecciones', function (/*inyecciones) {
+	  
+	}])*/
+    .run(function () {
+
+    });
